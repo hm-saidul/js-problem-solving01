1b58be8c4513d8ca1402a4c901225d14244a2923
saidzayan <saidulzayan@gmail.com>
2024-05-31 04:24:58 +0600

    first comment

---
+++ solve.js
@@ -0,0 +1,74 @@
+// problem 01
+// Create a variable called carName, assign the value Volvo to it.
+let carName = 'volvo';
+
+// problem 02
+// On one single line, declare three variables with the following names and values:
+var firstName = 'John',
+  lastName = 'Doe',
+  age = 35;
+
+// problem 03
+// Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+let x = 10;
+let y = 5;
+x *= y;
+console.log(x);
+
+// problem 04
+
+// let length = 16; // number
+// let lastName = 'Johnson'; // string
+
+// problem 05
+// function myFunction() {
+//   alert('Hello World!');
+// }
+
+// myFunction();
+
+// problem 06
+// const person = {
+//   name: 'John',
+//   age: 50,
+// };
+// alert(person.name + ' is ' + person.age);
+
+// problem 07
+// html page e
+
+// problem 08 (1)
+
+// const cars = ['Volvo', 'Jeep', 'Mercedes'];
+// alert(cars.length);
+
+// problem 08 (2)
+let brand = ['Volvo', 'Jeep', 'Mercedes'];
+brand.unshift('Ford');
+console.log(brand);
+
+// problem 09 (1)
+let a = Math.random();
+console.log(a);
+
+// problem 09 (2)
+
+const highest = Math.max(10, 20);
+console.log(highest);
+
+// problem 09 (3)
+
+const square = Math.sqrt(9);
+console.log(square);
+
+// problem 10 (1
+x = 10;
+y = 5;
+alert(x > y);
+
+// 10.comparison operator related problems!
+// number 2
+// let userAge = 18;
+// userAge < 18 ? console.log('you are young') : console.log('you are old');
+// const age = 19;
+// alert(age < 18 ? 'Too young' : 'Old enough');
